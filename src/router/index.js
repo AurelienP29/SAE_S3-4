49b216ae03884af9668f99d81d69f048a063647c
ValentinMICHELiutnfc <valentin.michel03@edu.univ-fcomte.fr>
2025-10-16 14:31:38 +0200

    page presta
    premier test pour page presta pour vérifier si mon implémentation rescpecte vue js et l'architecture du projet

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from "@/views/HomeView.vue";
+import PrestataireView from '@/views/PrestataireView.vue';
 
 // const routes = [
 //     {
@@ -11,6 +12,7 @@ import HomeView from "@/views/HomeView.vue";
 
 const routes = [
     { path: '/', name: 'Home', component: HomeView },
+    { path: '/prestataire', name: 'prestatairepage', component: PrestataireView}
 ]
 
 const router = createRouter({
---
+++ src/views/PrestataireView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="prestataire-container">
+    <h1 class="banner">Prestataire TEST</h1>
+    <p>Découvrez prestataire TEST.</p>
+
+
+
+    <p>Autres prestataires : </p>
+    <ul>
+      <li v-for="prestataire in prestataires" :key="prestataire.id">
+        {{ prestataire.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+</script>
